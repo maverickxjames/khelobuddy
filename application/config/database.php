87581f365edae7314d390acd8a959ef0f6aa8861
Jaykishan Rawat <jksonu1436@gmail.com>
2024-12-22 17:04:17 +0530

    GFGF

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '143.110.249.159',
 	'username' => 'khelobuddy',
 	'password' => 'khelobuddy',
 	'database' => 'khelobuddy',
